10a07d7f422e87440d541c16abaf4025646bf013
Mohamed <aharchi.mohamed@gmail.com>
2016-06-16 16:45:53 +0200

    Mise en place de la classe de test et la classe calcul

---
+++ Tests/Class1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using xCarpaccio.client;
+
+namespace Tests
+{
+    [TestFixture]
+    public class Class1
+    {
+        [Test]
+        public void NUnitIsWorking()
+        {
+            Assert.IsTrue(true);
+        }
+    }
+}
---
+++ extreme-carpaccio.client/Calcul.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace xCarpaccio.client
+{
+    class Calcul
+    {
+        private static string[] countries = {"DE", "UK", "FR", "IT", "ES", "PL", "RO", "NL", "BE", "EL", "CZ", "PT", "HU", "SE", "AT", "BG", "DK", "FI", "SK", "IE", "HR", "LT", "SI", "LV", "EE", "CY", "LU", "MT"};
+
+        public Calcul()
+        {
+            
+        }
+    }
+}
